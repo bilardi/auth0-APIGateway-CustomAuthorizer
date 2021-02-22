4831f724eb9f45957c8007cdafbe7943d43a9c2e
Alessandra Bilardi <alessandra.bilardi@pixartprinting.com>
2021-02-22 20:04:07 +0100

    added auth0 configuration

---
+++ reactJS/src/Auth/auth0-variables.js
@@ -0,0 +1,7 @@
+export const AUTH_CONFIG = {
+  domain: "pandle-net.eu.auth0.com",
+  clientId: "B4drVkYQdJNnI0nwwMgPG3QpmafZyMWs",
+  lcallbackUrl: "http://localhost:3000/callback",
+  pcallbackUrl: "https://bucket.domain.name.s3-website-eu-west-1.amazonaws.com/callback",
+  callbackUrl: "callback"
+}

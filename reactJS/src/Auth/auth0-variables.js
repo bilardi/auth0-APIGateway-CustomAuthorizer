d2c3244730000fa5fb6206cb6eaf237681346086
Alessandra Bilardi <alessandra.bilardi@pixartprinting.com>
2021-02-28 21:06:57 +0100

    update clientId

--- reactJS/src/Auth/auth0-variables.js
+++ reactJS/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: "pandle-net.eu.auth0.com",
-  clientId: "B4drVkYQdJNnI0nwwMgPG3QpmafZyMWs",
+  clientId: "ESF0BFvZCcymq4XUq825wmkgE3HIdoz1",
   lcallbackUrl: "http://localhost:3000/callback",
   pcallbackUrl: "https://bucket.domain.name.s3-website-eu-west-1.amazonaws.com/callback",
   callbackUrl: "callback"
